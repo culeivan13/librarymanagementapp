4a6422c470c78df56430e7ddf32284d5bb0e6606
Suraj Tiwari <surajtiwari2612@gmail.com>
2025-05-15 18:55:17 +0530

    Added Eureka Server and made all the services as eureka client

---
+++ MyDiscoveryServer/src/test/java/com/suraj/microservice/MyDiscoveryServer/MyDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraj.microservice.MyDiscoveryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
