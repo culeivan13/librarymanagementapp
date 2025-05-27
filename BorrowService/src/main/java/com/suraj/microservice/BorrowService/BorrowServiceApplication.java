2a93e11360db5a58c3f1c1db300737d0a8c0591a
Suraj Tiwari <surajtiwari2612@gmail.com>
2025-05-27 12:09:37 +0530

    Added passwords to .env file

--- BorrowService/src/main/java/com/suraj/microservice/BorrowService/BorrowServiceApplication.java
+++ BorrowService/src/main/java/com/suraj/microservice/BorrowService/BorrowServiceApplication.java
@@ -1,5 +1,6 @@
 package com.suraj.microservice.BorrowService;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BorrowServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(BorrowServiceApplication.class, args);
 	}
 
--- UserService/src/main/java/com/suraj/microservice/userservice/UserServiceApplication.java
+++ UserService/src/main/java/com/suraj/microservice/userservice/UserServiceApplication.java
@@ -1,5 +1,6 @@
 package com.suraj.microservice.userservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,6 +10,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
